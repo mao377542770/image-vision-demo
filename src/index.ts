8babb27a1c3b71baf3c38cf41373932291078e1b
mao377542770 <377542770@qq.com>
2021-11-05 17:40:06 +0900

    port 変更

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import App from "./app.server";
 import { FilesController } from "./controller/files.controller";
 
+const port = process.env.PORT || 3000;
+
 const app = new App(
   [
     new FilesController()
   ],
-  3000
+  port as number
 );
 
 app.listen();
